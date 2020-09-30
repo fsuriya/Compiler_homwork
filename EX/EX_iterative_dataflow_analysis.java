8d82954940cdd8363c65daa1f856030b96ca79c5
fsuriya <flukbanrong@gmail.com>
2020-10-01 01:06:06 +0700

    EX_IDA

--- EX/EX_iterative_dataflow_analysis.java
+++ EX/EX_iterative_dataflow_analysis.java
@@ -2,18 +2,19 @@ import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
 
-class IDA{
-    public static void main(String[] args) {
-        try{
-            File myFile = new File("state.txt");
-            Scanner Input = new Scanner(myFile);
-            while(Input.hasNextLine()){
-                String data = Input.nextLine();
-                System.out.println(data);
-            }
-        } catch (FileNotFoundException e){
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-    }
+public class ReadFile {
+  public static void main(String[] args) {
+    // try {
+      File myObj = new File("state.txt");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    // } catch (FileNotFoundException e) {
+    //   System.out.println("An error occurred.");
+    //   e.printStackTrace();
+    // }
+  }
 }
---
+++ EX/IDA.py
@@ -0,0 +1,68 @@
+# Python cann't union first set is empty set
+from typing import Counter
+
+
+def newUnion(A, B):
+    setA = A.copy()
+    setB = B.copy()
+    if setA != {}:
+        return setA.union(setB)
+    elif setB != {}:
+        return setB.union(setA)
+    else:
+        return {}
+
+def newMinus(A, B):
+    setA = A.copy()
+    setB = B.copy()
+    if (setA != {}) and (setB != {}):
+        return setA - setB
+    elif (setA != {}) and (setB == {}):
+        return setA
+    elif (setA == {}):
+        return {}
+    
+
+# Create dataflow
+# -1 is empty
+next = [[2], [3, 4], [5], -1, [6], [7, 8], [2], [2]]
+use = [{}, {"x"}, {"e"}, {"x"}, {"e", "x"}, {"x"}, {"z"}, {"y"}]
+deff = [{"e"}, {}, {"z"}, {}, {"y"}, {}, {"e"}, {"e"}]
+
+if (len(next) == len(use)) and (len(next) == len(deff)):
+    n = len(next)
+else:
+    print("Wrong input!!!")
+
+# Start with in[n] = out[n] = empty
+inn = [{}] * n
+out = [{}] * n
+
+# Init temp inOld and outOld
+inOld = []
+outOld = []
+
+while not((inn == inOld) and (out == outOld)):
+    # Temp for store in_Old and out_Old
+    inOld = inn.copy()
+    outOld = out.copy()
+
+    for i in range(n):
+        # out[n] = allConnect in[n']
+        out[i] = {}
+        if next[i] != -1:
+            for j in next[i]:
+                out[i] = newUnion(out[i], inn[j-1])
+        
+        # in[n] = use [n] U (out[n] - def[n])
+        usecopy = use.copy()
+        outcopy = out.copy()
+        inn[i] = newUnion(usecopy[i], newMinus(outcopy[i], deff[i]))
+    
+    counter = 1
+    print(str(counter) + " : ")
+    print("Out")
+    print(out)
+    print("In")
+    print(inn)
+    counter = counter + 1
